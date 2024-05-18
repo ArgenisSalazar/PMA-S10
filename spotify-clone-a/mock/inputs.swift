5202f470164f062b5b96972119bc8f4b1aaed16f
Mac21 <alumno@MacBook-Pro-21.local>
2024-05-18 14:53:20 -0500

    first commit

---
+++ spotify-clone-a/mock/inputs.swift
@@ -0,0 +1,14 @@
+//
+//  inputs.swift
+//  spotify-clone-a
+//
+//  Created by Mac21 on 18/05/24.
+//
+
+import Foundation
+
+struct SignUpForm{
+    var title: String
+    var input: String
+    var helText: String
+}
